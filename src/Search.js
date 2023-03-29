e7854bd7085dbffe7e985ebb9390110f15c71a4d
Erica Chambers <53728223+ebc1201@users.noreply.github.com>
2023-03-29 16:40:53 -0500

    Added Axios and dictionary API

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import './Search.css';
+import axios from 'axios';
 
 export default function Search() {
   let [keyword, setKeyword] = useState('');
+
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition...`);
+
+    const apiKey = '5ae36e7a40754bfb55o3c43890a696t8';
+    const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
